d2c72f1271f8ebf07727070b1a2b6af8ddd8ce22
pangwu86 <pangwu86@gmail.com>
2016-08-22 23:32:59 +0800

    update:文档页添加affix

--- web/assets/js/docment.js
+++ web/assets/js/docment.js
@@ -1,3 +1,12 @@
 $(document).ready(function () {
 
+    // $('#doc-affix').affix({
+    //     offset: {
+    //         top: 100,
+    //         bottom: function () {
+    //             return (this.bottom = $('.footer').outerHeight(true))
+    //         }
+    //     }
+    // })
+
 });
