de9e828c8db790eedd6e6c61579f4ac20a4fc918
pangwu86 <pangwu86@gmail.com>
2016-08-23 00:06:36 +0800

    add:下载页面基本搞定，还差兼容性列表

--- web/assets/js/docment.js
+++ web/assets/js/docment.js
@@ -1,12 +1,3 @@
 $(document).ready(function () {
 
-    // $('#doc-affix').affix({
-    //     offset: {
-    //         top: 100,
-    //         bottom: function () {
-    //             return (this.bottom = $('.footer').outerHeight(true))
-    //         }
-    //     }
-    // })
-
 });
--- web/assets/js/download.js
+++ web/assets/js/download.js
@@ -1,3 +1,9 @@
 $(document).ready(function () {
-
+    // btn 下载
+    $('.app_xplay').click(function () {
+        window.location.href = "xplay.apk";
+    });
+    $('.app_kingroot').click(function () {
+        window.location.href = "kingroot.apk";
+    });
 });
