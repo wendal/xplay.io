570195e2405ce4a2b2db7638061bca70ed19dfd9
pangwu86 <pangwu86@gmail.com>
2016-09-06 14:58:33 +0800

    fix:win预览版

--- web/assets/js/download.js
+++ web/assets/js/download.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
     // btn 下载
     $('.software_download').click(function () {
         var nm = $(this).attr('snm');
-        window.location.href = "xplay.apk";
+        window.location.href = nm;
     });
 });
