987656c1fbd286f25761eeb14645f9a944f63c08
pangwu86 <pangwu86@gmail.com>
2017-03-13 16:48:08 +0800

    ..

--- web/assets/js/index.js
+++ web/assets/js/index.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
     // btn 跳转
 
     $('.btn.page_document').click(function () {
-        window.location.href = "document.html";
+        window.location.href = "document_xplay_android.html";
     });
     $('.btn.page_download').click(function () {
         window.location.href = "download.html";
