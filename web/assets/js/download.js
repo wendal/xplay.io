a50466d5502c1c33bfe4cb2d41671a1687fbfdb4
pangwu86 <pangwu86@gmail.com>
2017-03-13 17:39:58 +0800

    add:几个需要下载的文件，直接放到git库里了

--- web/assets/js/download.js
+++ web/assets/js/download.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
     // btn 下载
-    $('.software_download').click(function () {
-        var nm = $(this).attr('snm');
-        window.location.href = nm;
-    });
+    // $('.software_download').click(function () {
+    //     var nm = $(this).attr('snm');
+    //     window.location.href = nm;
+    // });
 });
